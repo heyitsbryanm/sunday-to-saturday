f206ae1887cfb5d619067a728d72303b507dcdd0
Bryan Matias <matiasbryan@gmail.com>
2021-08-11 15:32:13 -0700

    Adds debugging options

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-satToSun = (str) => {
+satToSun = (str, options) => {
+    options = {
+        ...{
+            debug: false
+        }, ...options
+    }
     str = str.trim()
     const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
@@ -22,6 +27,9 @@ satToSun = (str) => {
     // if there is a dash, it's a range. Let's get the range of dates
     // we use `0` because it should never begin with a `-`
     if (str.indexOf("-") > 0) {
+        if (!!options.debug) {
+            console.debug(`"-" detected`)
+        }
         str = str.split("-");
         str = str.map(x => x.trim().toLowerCase())
 
@@ -47,20 +55,43 @@ satToSun = (str) => {
         // we use `0` cause it should never begin with a `,`
         result = splitAndFind(str, ",")
     } else if (str.indexOf(" ") > 0) {
+        if (!!options.debug) {
+            console.debug(`space detected`)
+        }
         // if there's a space only, it's a list. Let's treat it like one
         // we use `0` cause it should never begin with a ` `
         result = splitAndFind(str, ' ')
     } else if (str.indexOf('weekend') > -1 || str.indexOf("wknd") > -1 || str.indexOf("end") > -1) {
+
+        if (!!options.debug) {
+            console.debug(`"weekend" detected`)
+        }
+
         result = ["sunday", "saturday"]
     } else if (str.indexOf('weekday') > -1 || str.indexOf("wkday") > -1 || str.indexOf("workday") > -1) {
+
+        if (!!options.debug) {
+            console.debug(`"weekday" detected`)
+        }
+
         result = [...days].splice(1, 5)
     } else if (!!str) {
+
+        if (!!options.debug) {
+            console.debug(`single string detected`)
+        }
+
         // if nothing else, that means there's just a single value. In this case, reeturn a single day
         result.push(days.find(x => x.indexOf(str) > -1))
     }
 
     // cleanup functions.
     if (result.length === 1 && !!result[0]) {
+
+        if (!!options.debug) {
+            console.debug(`No type detected. Value is: ${str}`)
+        }
+
         // returns the single value if that's all there is
         return result
     } else if (!!result) {
